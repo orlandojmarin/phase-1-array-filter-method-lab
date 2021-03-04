a62ecf7a9bff4963961903ae8ca5e6f9600d87c9
Orlando Marin <orlandojermainemarin@gmail.com>
2021-03-04 11:50:35 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+//FIND MATCHING FUNCTION
+function findMatching(driverNames, string) {
+    return driverNames.filter( (driverName) => {
+        return driverName.toLowerCase() == string.toLowerCase()
+    });
+}
+
+//FUZZY MATCH FUNCTION
+function fuzzyMatch(driverNames, name) {
+    return driverNames.filter( (driver) => {
+        return driver[0] == name[0]
+    })
+}
+
+//MATCH NAME FUNCTION
+function matchName(driverObjects, string) {
+    return driverObjects.filter( (object) => {
+        return object.name == string
+    })
+}
